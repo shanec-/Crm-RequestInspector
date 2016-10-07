cb97c1e161c0d75a6415a6ef68be8c5320c72ebe
Shane Carvalho <shanec_@hotmail.com>
2016-10-07 14:30:06 +1100

    Initial commit

---
+++ src/CrmRequestInspector/CustomMessageInspector.cs
@@ -0,0 +1,55 @@
+﻿using Serilog;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+
+namespace CrmRequestInspector
+{
+    public class CustomMessageInspector : IClientMessageInspector, IEndpointBehavior
+    {
+        private ILogger log;
+
+        public CustomMessageInspector(ILogger log)
+        {
+            this.log = log;
+        }
+
+        #region IClientMessageInspector Members
+
+        public void AfterReceiveReply(ref System.ServiceModel.Channels.Message reply, object correlationState)
+        {
+            this.log.Debug("Received Message: {reply}", reply);
+        }
+
+        public object BeforeSendRequest(ref System.ServiceModel.Channels.Message request, IClientChannel channel)
+        {
+            this.log.Debug("Sent Message: {request}", request);
+            return null;
+        }
+
+        #endregion
+
+
+
+        #region IEndpointBehavior Members
+
+        public void AddBindingParameters(ServiceEndpoint endpoint, System.ServiceModel.Channels.BindingParameterCollection bindingParameters)
+        {
+        }
+
+        public void ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime)
+        {
+            clientRuntime.MessageInspectors.Add(this);
+        }
+
+        public void ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)
+        {
+        }
+
+        public void Validate(ServiceEndpoint endpoint)
+        {
+        }
+
+        #endregion
+    }
+}
---
+++ src/CrmRequestInspector/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Configuration;
+using Microsoft.Xrm.Client;
+using Microsoft.Xrm.Sdk.Client;
+using Serilog;
+
+namespace CrmRequestInspector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InitializeLog();
+            ExecuteOperation();
+        }
+
+        public static void InitializeLog()
+        {
+            string logFolderPath = ConfigurationManager.AppSettings["LogFolderPath"];
+
+            if (string.IsNullOrEmpty(logFolderPath))
+            {
+                logFolderPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            }
+
+            var loggerConfiguration =
+                new LoggerConfiguration()
+                    .WriteTo.RollingFile(string.Concat(logFolderPath, @"\Log-{Date}.txt"));
+
+            loggerConfiguration.MinimumLevel.Debug();
+
+            Log.Logger = loggerConfiguration.CreateLogger();
+        }
+
+        public static void ExecuteOperation()
+        {
+            string crmConnectionString = ConfigurationManager.ConnectionStrings["crm"].ConnectionString;
+            var crmConnection = new CrmConnection(new ConnectionStringSettings("Crm", crmConnectionString));
+
+            using (var organizationService = CreateProxy(crmConnection))
+            {
+                var retrieveOperation = new RetrieveOperation(organizationService, Log.Logger);
+                retrieveOperation.Execute();
+            }
+        }
+
+
+        public static OrganizationServiceProxy CreateProxy(CrmConnection connection)
+        {
+            var serviceProxy = new OrganizationServiceProxy(connection.ServiceUri, connection.HomeRealmUri,connection.ClientCredentials, connection.DeviceCredentials);
+
+            var inspector = new CustomMessageInspector(Log.Logger);
+            serviceProxy.ServiceConfiguration.CurrentServiceEndpoint.Behaviors.Add(inspector);
+
+            return serviceProxy;
+        }
+    }
+}
